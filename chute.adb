fd724ea4292c7dcd2196c46a0af32e0441148baa
Donal Cahill <magicmole@gmail.com>
2013-03-01 16:40:45 +0000

    Add empty chute.adb.

---
+++ chute.adb
@@ -0,0 +1,34 @@
+with Register_Control;
+use Register_Control;
+
+package body Chute is
+   procedure Hopper_Load is
+   begin
+      null;
+   end Hopper_Load;
+   
+   procedure Hopper_Unload is
+   begin
+      null;
+   end Hopper_Unload;
+   
+   procedure Sorter_Metal is
+   begin
+      null;
+   end Sorter_Metal;
+   
+   procedure Sorter_Glass is
+   begin
+      null;
+   end Sorter_Glass;
+   
+   procedure Sorter_Close is
+   begin
+      null;
+   end Sorter_Close;
+   
+   procedure Get_Ball(B: out Ball_Detecded; T: out Time) is
+   begin
+      null;
+   end Get_Ball;
+end Chute;
